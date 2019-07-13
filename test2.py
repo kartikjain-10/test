a94ff0d38ed0124ed27e19dec2a96799fce658da
kartikjain-10 <kartikjain996@gmail.com>
2019-07-13 23:58:53 +0530

    make it rain

--- test2.py
+++ test2.py
@@ -1,1 +1,3 @@
-print('I don't know what i'm chasing, maybe i'll never know')
+print("I don't know what i'm chasing, maybe i'll never know")
+print("I don't know what im chasing, maybe i'll never know")
+print('aniket is a lover boy')
